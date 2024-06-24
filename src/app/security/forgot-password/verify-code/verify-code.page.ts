e1c5195d56bed1b0d1f86928b1851f15bc3ab23d
alejandroamesty <alejandro.30337890@uru.edu>
2024-06-23 21:59:50 -0400

    feat: crear nuevos componentes y pantallas

--- src/app/security/forgot-password/verify-code/verify-code.page.ts
+++ src/app/security/forgot-password/verify-code/verify-code.page.ts
@@ -1,22 +1,23 @@
 import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
-import { NavController } from '@ionic/angular';
 import {
   IonContent,
   IonHeader,
   IonTitle,
   IonToolbar,
 } from '@ionic/angular/standalone';
+import { NavController } from '@ionic/angular';
 
 import { BorderInputComponent } from 'src/components/border-input/border-input.component';
 import { ControlButtonComponent } from 'src/components/control-button/control-button.component';
 import { SaveButtonComponent } from 'src/components/save-button/save-button.component';
+import { VerificationInputComponent } from 'src/components/verification-input/verification-input.component';
 
 @Component({
-  selector: 'app-forgot-password',
-  templateUrl: './forgot-password.page.html',
-  styleUrls: ['./forgot-password.page.scss'],
+  selector: 'app-verify-code',
+  templateUrl: './verify-code.page.html',
+  styleUrls: ['./verify-code.page.scss'],
   standalone: true,
   imports: [
     IonContent,
@@ -28,29 +29,30 @@ import { SaveButtonComponent } from 'src/components/save-button/save-button.comp
     BorderInputComponent,
     ControlButtonComponent,
     SaveButtonComponent,
+    VerificationInputComponent
   ],
 })
-export class ForgotPasswordPage implements OnInit {
+export class VerifyCodePage implements OnInit {
   constructor(private navCtrl: NavController) {}
 
   ngOnInit() {}
 
-  navigateToSignIn() {
-    this.navCtrl.navigateForward('sign-in', {
+  navigateToForgotPassword() {
+    this.navCtrl.navigateBack('forgot-password', {
       animated: true,
       animationDirection: 'back',
     });
   }
 
-  navigateToVerifyCode() {
-    this.navCtrl.navigateForward('verify-code', {
+  navigateToSignUp() {
+    this.navCtrl.navigateBack('sign-up', {
       animated: true,
-      animationDirection: 'forward',
+      animationDirection: 'back',
     });
   }
 
-  navigateToSignUp() {
-    this.navCtrl.navigateBack('sign-up', {
+  navigateToResetPassword() {
+    this.navCtrl.navigateForward('reset-password', {
       animated: true,
       animationDirection: 'forward',
     });
