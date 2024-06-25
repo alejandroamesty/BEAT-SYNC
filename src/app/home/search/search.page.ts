e6556f846aec6f3c2789298f31d8ec16b66369e6
alejandroamesty <alejandro.30337890@uru.edu>
2024-06-25 01:30:04 -0400

    feat: crear componente y pantalla de búsqueda

--- src/app/home/search/search.page.ts
+++ src/app/home/search/search.page.ts
@@ -1,21 +1,42 @@
 import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
-import { IonContent, IonHeader, IonTitle, IonToolbar, IonButtons } from '@ionic/angular/standalone';
+import {
+  IonContent,
+  IonHeader,
+  IonTitle,
+  IonToolbar,
+  IonButtons,
+} from '@ionic/angular/standalone';
 import { CategoryCardComponent } from 'src/components/category-card/category-card.component';
+import { SearchTracksPage } from './search-tracks/search-tracks.page';
+import { Router } from '@angular/router';
+import {Location} from '@angular/common';
 
 @Component({
   selector: 'app-search',
   templateUrl: './search.page.html',
   styleUrls: ['./search.page.scss'],
   standalone: true,
-  imports: [IonButtons, IonContent, IonHeader, IonTitle, IonToolbar, CommonModule, FormsModule, CategoryCardComponent]
+  imports: [
+    IonButtons,
+    IonContent,
+    IonHeader,
+    IonTitle,
+    IonToolbar,
+    CommonModule,
+    FormsModule,
+    CategoryCardComponent,
+    SearchTracksPage,
+  ],
 })
 export class SearchPage implements OnInit {
+  showSearchTracks: boolean = false;
+  constructor(private router: Router, private _location: Location) {}
 
-  constructor() { }
+  ngOnInit() {}
 
-  ngOnInit() {
+  handleTrackCardPress() {
+    this.router.navigate(['search-tracks']);
   }
-
 }
